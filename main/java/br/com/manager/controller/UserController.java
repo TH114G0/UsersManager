21bf43f82962541f552c6c6bd5d49664c44e2ace
Thiago Jose <thiago.wilton2704@gmail.com>
2025-02-06 14:07:00 -0300

    chore: remover imports não utilizados e imports redundantes

--- main/java/br/com/manager/controller/UserController.java
+++ main/java/br/com/manager/controller/UserController.java
@@ -1,6 +1,5 @@
 package br.com.manager.controller;
 
-import br.com.manager.domain.model.Users;
 import br.com.manager.dto.CreateUserDTO;
 import br.com.manager.dto.UserResponseDTO;
 import br.com.manager.service.UserService;
@@ -8,7 +7,10 @@ import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
 @RequestMapping("/user")
