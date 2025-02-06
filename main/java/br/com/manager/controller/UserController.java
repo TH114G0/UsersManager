a1e9cd10f8bf200fc6f978e53fc4b767689b00be
Thiago Jose <thiago.wilton2704@gmail.com>
2025-02-06 14:03:10 -0300

    feat: adicionar UserController para criação de usuário

---
+++ main/java/br/com/manager/controller/UserController.java
@@ -0,0 +1,25 @@
+package br.com.manager.controller;
+
+import br.com.manager.domain.model.Users;
+import br.com.manager.dto.CreateUserDTO;
+import br.com.manager.dto.UserResponseDTO;
+import br.com.manager.service.UserService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping
+    public ResponseEntity<UserResponseDTO> create(@RequestBody @Valid CreateUserDTO createUserDTO) {
+        var user = userService.create(createUserDTO);
+        return ResponseEntity.status(HttpStatus.CREATED).body(user);
+    }
+}
