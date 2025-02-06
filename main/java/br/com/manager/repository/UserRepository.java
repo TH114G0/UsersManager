0e020a2126debb2364e67c08bd939b24ed6263e9
Thiago Jose <thiago.wilton2704@gmail.com>
2025-02-06 17:02:11 -0300

    feat: adicionar UserRepository para operações no banco de dados

---
+++ main/java/br/com/manager/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package br.com.manager.repository;
+
+import br.com.manager.domain.model.Users;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<Users, Long> { }
