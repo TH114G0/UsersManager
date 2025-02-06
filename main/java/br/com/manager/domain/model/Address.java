1fdaefd55fa46db5d3b63237f7fe10381749d18f
Thiago Jose <thiago.wilton2704@gmail.com>
2025-02-06 14:09:03 -0300

    feat: adicionar classe Address como embeddable para endereço do usuário

---
+++ main/java/br/com/manager/domain/model/Address.java
@@ -0,0 +1,43 @@
+package br.com.manager.domain.model;
+
+import br.com.manager.dto.AddressDTO;
+import jakarta.persistence.Embeddable;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Embeddable
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class Address {
+
+    private String city;
+
+    private String street;
+
+    private String state;
+
+    private String neighborhood;
+
+    private String state_code;
+
+    private String zip_code;
+
+    private String complement;
+
+    private String number;
+
+    public Address(AddressDTO address) {
+        this.city = address.getCity();
+        this.street = address.getStreet();
+        this.state = address.getState();
+        this.state_code = address.getState_code();
+        this.neighborhood = address.getNeighborhood();
+        this.zip_code = address.getZip_code();
+        this.complement = address.getComplement();
+        this.number = address.getNumber();
+    }
+}
