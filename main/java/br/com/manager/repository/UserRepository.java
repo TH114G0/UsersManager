29c6da0eec7352f7464aee1909a5e26428f6fd4e
Thiago Jose <thiago.wilton2704@gmail.com>
2025-02-07 17:14:16 -0300

    feat: implementar listagem paginada de usuários ativos

--- main/java/br/com/manager/repository/UserRepository.java
+++ main/java/br/com/manager/repository/UserRepository.java
@@ -1,8 +1,12 @@
 package br.com.manager.repository;
 
 import br.com.manager.domain.model.Users;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepository extends JpaRepository<Users, Long> { }
+public interface UserRepository extends JpaRepository<Users, Long> {
+    Page<Users> findAllByActiveTrue(Pageable pageable);
+}
