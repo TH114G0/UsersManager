5d675e8ecb6d02c01457add644454cc336b96cfc
Thiago Jose <thiago.wilton2704@gmail.com>
2025-02-07 16:56:53 -0300

    feat: adicionar endpoint GET para listar usuários paginados

--- main/java/br/com/manager/controller/UserController.java
+++ main/java/br/com/manager/controller/UserController.java
@@ -1,16 +1,17 @@
 package br.com.manager.controller;
 
 import br.com.manager.dto.CreateUserDTO;
+import br.com.manager.dto.UserListDTO;
 import br.com.manager.dto.UserResponseDTO;
 import br.com.manager.service.UserService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/user")
@@ -23,5 +24,11 @@ public class UserController {
     public ResponseEntity<UserResponseDTO> create(@RequestBody @Valid CreateUserDTO createUserDTO) {
         var user = userService.create(createUserDTO);
         return ResponseEntity.status(HttpStatus.CREATED).body(user);
+    }
+
+    @GetMapping
+    public ResponseEntity<Page<UserListDTO>> list(@PageableDefault(size = 10, sort = {"name"}) Pageable pageable) {
+        var page = userService.list(pageable);
+        return ResponseEntity.ok(page);
     }
 }
