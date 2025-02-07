59c1b19cf474149de1a4bc0180d3c3212793f996
Thiago Jose <thiago.wilton2704@gmail.com>
2025-02-07 17:35:44 -0300

    feat: adicionar @Transactional no create e implementar listagem de usuários ativos

--- main/java/br/com/manager/service/UserService.java
+++ main/java/br/com/manager/service/UserService.java
@@ -2,9 +2,13 @@ package br.com.manager.service;
 
 import br.com.manager.domain.model.Users;
 import br.com.manager.dto.CreateUserDTO;
+import br.com.manager.dto.UserListDTO;
 import br.com.manager.dto.UserResponseDTO;
 import br.com.manager.repository.UserRepository;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -13,8 +17,14 @@ public class UserService {
     @Autowired
     private UserRepository userRepository;
 
+    @Transactional
     public UserResponseDTO create(CreateUserDTO createUserDTO) {
         var user = userRepository.save(new Users(createUserDTO));
         return new UserResponseDTO(user);
+    }
+
+    @Transactional
+    public Page<UserListDTO> list(Pageable pageable) {
+         return userRepository.findAllByActiveTrue(pageable).map(UserListDTO::new);
     }
 }
