221d379912cfacc30fce017acb095b66b6361268
Thiago Jose <thiago.wilton2704@gmail.com>
2025-02-08 13:52:24 -0300

    feat: adicionar métodos de atualização e exclusão com tratamento de exceção para usuários não encontrados.

--- main/java/br/com/manager/service/UserService.java
+++ main/java/br/com/manager/service/UserService.java
@@ -4,7 +4,9 @@ import br.com.manager.domain.model.Users;
 import br.com.manager.dto.CreateUserDTO;
 import br.com.manager.dto.UserListDTO;
 import br.com.manager.dto.UserResponseDTO;
+import br.com.manager.dto.UserUpdateDTO;
 import br.com.manager.repository.UserRepository;
+import jakarta.persistence.EntityNotFoundException;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -26,5 +28,20 @@ public class UserService {
     @Transactional
     public Page<UserListDTO> list(Pageable pageable) {
          return userRepository.findAllByActiveTrue(pageable).map(UserListDTO::new);
+    }
+
+    @Transactional
+    public UserResponseDTO update(UserUpdateDTO userUpdateDTO) {
+        var user = userRepository.findById(userUpdateDTO.getId()).
+                orElseThrow(() -> new EntityNotFoundException("Usuário não encontrado."));
+        user.update(userUpdateDTO);
+        return new UserResponseDTO(user);
+    }
+
+    @Transactional
+    public void delete(Long id) {
+        var user = userRepository.findById(id).
+                orElseThrow(() -> new EntityNotFoundException("Usuário não encontrado."));
+        user.delete();
     }
 }
