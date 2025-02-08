a84de093001e61817676724858978357b7f0e972
Thiago Jose <thiago.wilton2704@gmail.com>
2025-02-08 13:34:03 -0300

    feat: adicionar método update para atualizar apenas campos fornecidos no AddressDTO

--- main/java/br/com/manager/domain/model/Address.java
+++ main/java/br/com/manager/domain/model/Address.java
@@ -40,4 +40,31 @@ public class Address {
         this.complement = address.getComplement();
         this.number = address.getNumber();
     }
+
+    public void update(AddressDTO addressDTO) {
+        if (addressDTO.getCity() != null && !addressDTO.getCity().isBlank()) {
+            this.city = addressDTO.getCity();
+        }
+        if (addressDTO.getStreet() != null && !addressDTO.getStreet().isBlank()) {
+            this.street = addressDTO.getStreet();
+        }
+        if (addressDTO.getState() != null && !addressDTO.getState().isBlank()) {
+            this.state = addressDTO.getState();
+        }
+        if (addressDTO.getState_code() != null && !addressDTO.getState_code().isBlank()) {
+            this.state_code = addressDTO.getState_code();
+        }
+        if (addressDTO.getNeighborhood() != null && !addressDTO.getNeighborhood().isBlank()) {
+            this.neighborhood = addressDTO.getNeighborhood();
+        }
+        if (addressDTO.getZip_code() != null && !addressDTO.getZip_code().isBlank()) {
+            this.zip_code = addressDTO.getZip_code();
+        }
+        if (addressDTO.getComplement() != null && !addressDTO.getComplement().isBlank()) {
+            this.complement = addressDTO.getComplement();
+        }
+        if (addressDTO.getNumber() != null && !addressDTO.getNumber().isBlank()) {
+            this.number = addressDTO.getNumber();
+        }
+    }
 }
